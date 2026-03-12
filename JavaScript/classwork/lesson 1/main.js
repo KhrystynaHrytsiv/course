75175c51bd37ded9fbec194b71b8b422b159d7b8
KhrystynaHrytsiv <khrystyna.hrytsiv@gmail.com>
2026-03-12 21:25:29 +0200

    js lesson 1

---
+++ JavaScript/classwork/lesson 1/main.js
@@ -0,0 +1,22 @@
+// console.log("asd");
+// {
+//     let a = 'regv';
+//     var b =5163
+// }
+// console.log(a)
+// let a = 123
+// console.log(a)
+//
+// const PI = 3.14;
+//
+//     a = 'svfsdc';
+// console.log(a);
+////////////////////////////
+let aq = 'hello';
+console.log(typeof a);
+
+let b = 100500;
+console.log( typeof b, b);
+
+let c = true;
+console.log(c);
---
+++ JavaScript/homework/main.js
@@ -0,0 +1,52 @@
+// Створити змінні. Присвоїти кожному з них значення: ‘hello’,’owu’,’com’, ‘ua’, 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// let a = 'hello';
+// let b = 'owu';
+// let c = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let j = 3.14;
+let k = 2.7;
+let l = 16;
+let m = true;
+let n = false;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+console.log(n);
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+let firstName = 'Khrystyna ';
+let middleName = 'Romanivna';
+let lastName = 'Hrytsiv';
+console.log(firstName + middleName + " " + lastName);
+
+// За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+let name = prompt("Enter your ame");
+let midName = prompt("Enter your middleName");
+let age = prompt("Enter your age");
+
+console.log("Name:", name);
+console.log("MiddleName:", midName);
+console.log("Age:", age);
