ee1d289d3685d8f574c3f1d5f1e6ff5dd1eca1f4
KhrystynaHrytsiv <khrystyna.hrytsiv@gmail.com>
2026-03-18 17:33:05 +0200

    hw 7

---
+++ JavaScript/homework/hw7/hw7_6/main.js
@@ -0,0 +1,44 @@
+// - Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску,
+// максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
+// -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
+// -- info () - яка виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
+// -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
+// -- changeYear (newValue) - змінює рік випуску на значення newValue
+// -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
+
+function Car (model, producer, year, maxSpeed, volume){
+    this.model = model;
+    this.producer = producer;
+    this.year = year;
+    this.maxSpeed = maxSpeed;
+    this.volume = volume;
+    this.drive = function (){
+        console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`);
+    };
+    this.info = function (){
+        for (const key in this) {
+            console.log(key, this[key]);
+        }
+    };
+    this.increaseMaxSpeed = function (newSpeed){
+        if (newSpeed> 0) this.maxSpeed = newSpeed
+    };
+    this.changeYear = function (newValue) {
+        if (newValue > 1815) this.year = newValue;
+    }
+    this.addDriver = function (driver){
+        if (driver) this.driver = driver
+    }
+}
+let car = new Car('porsche', 'Germany', 2022, 320, 450);
+console.log(car);
+car.drive();
+car.info()
+car.increaseMaxSpeed(350);
+console.log(car);
+car.changeYear(2023);
+console.log(car);
+car.addDriver({})
+console.log(car);
+
+
---
+++ JavaScript/homework/hw7/hw7_9/main.js
@@ -0,0 +1,34 @@
+// *Через Array.prototype. створити власний foreach, filter
+
+Array.prototype.ownForEach = function (callback){
+    let arr = this;
+    for (let item of arr){
+        callback(item)
+    }
+}
+let nums = [11, 22, 33, 44];
+nums.ownForEach((a) => console.log(a))
+
+Array.prototype.ownFilter = function (predicate){
+    let arr = [];
+    for (const item of this) {
+        if (predicate(item)){
+            arr.push(item)
+        }
+    }
+    return arr
+}
+let users = [
+            {name: 'vasya', age: 31, status: false},
+            {name: 'petya', age: 30, status: true},
+            {name: 'kolya', age: 29, status: true},
+            {name: 'olya', age: 28, status: false},
+            {name: 'max', age: 30, status: true},
+            {name: 'anya', age: 31, status: false},
+            {name: 'oleg', age: 28, status: false},
+            {name: 'andrey', age: 29, status: true},
+            {name: 'masha', age: 30, status: true},
+            {name: 'yulia', age: 31, status: false},
+            {name: 'ivan', age: 31, status: true}
+        ];
+console.log(users.ownFilter((user) => !user.status));
