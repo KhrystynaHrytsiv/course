7cb7972844d8ff125a8ef8790cd1826377cff7ad
KhrystynaHrytsiv <khrystyna.hrytsiv@gmail.com>
2026-03-25 18:05:10 +0200

    hw 11

---
+++ JavaScript/homework/hw11/hw11_1/main.js
@@ -0,0 +1,40 @@
+// взяти https://dummyjson.com/docs/carts та вивести інформацію про всі корзини. Відобразити всі поля кожної корзини.
+let container = document.createElement('div');
+container.classList.add('container');
+fetch('https://dummyjson.com/carts')
+    .then(res => res.json())
+    .then(({carts}) =>{
+for (let cart of carts) {
+           const {id, products, total, discountedTotal, userId, totalProducts, totalQuantity} = cart;
+           let cartDiv = document.createElement('div');
+           cartDiv.classList.add('cart')
+           let idDiv = document.createElement('div');
+           idDiv.innerText = `Cart number : ${id}`
+
+           let productsDiv = document.createElement('ul');
+           productsDiv.innerText = 'Products:'
+           for (let product of products){
+               let title = document.createElement('li');
+               title.innerText = product.title
+               productsDiv.appendChild(title)
+           }
+
+           let totalDiv = document.createElement('div');
+           totalDiv.innerText = `Total sum: ${total}`;
+
+           let discountedTotalDiv = document.createElement('div');
+           discountedTotalDiv.innerText = `Total discount: ${discountedTotal}`;
+           let userIdDiv = document.createElement('div');
+           userIdDiv.innerText = `UserId: ${userId}`;
+
+           let totalProductsDiv = document.createElement('div');
+           totalProductsDiv.innerText = `Total products: ${totalProducts}`;
+
+           let totalQuantityDiv = document.createElement('div');
+           totalQuantityDiv.innerText = `Total quantity: ${totalQuantity}`;
+           cartDiv.append(idDiv, productsDiv, totalDiv, discountedTotalDiv, totalProductsDiv, totalQuantityDiv)
+            container.appendChild(cartDiv)
+        }
+
+    });
+document.body.appendChild(container)
---
+++ JavaScript/homework/hw11/hw11_2/main.js
@@ -0,0 +1,45 @@
+fetch('https://dummyjson.com/recipes')
+    .then(res => res.json())
+    .then(({recipes}) =>{
+        let container = document.createElement('div');
+        container.classList.add('container')
+        for (const recipe of recipes) {
+            let recipeBlock = document.createElement('div');
+            recipeBlock.classList.add('recipe')
+            const{name, ingredients, instructions, prepTimeMinutes, cookTimeMinutes, servings, difficulty, cuisine, caloriesPerServing, image, rating, reviewCount, mealType} = recipe;
+            let title = document.createElement('h2');
+            title.innerText = name;
+
+            let ingredientsUl = document.createElement('ul');
+            ingredientsUl.innerText = 'Ingredients:'
+            for (let ing of ingredients){
+                let ingredient = document.createElement('li');
+                ingredient.innerText = ing;
+                ingredientsUl.appendChild(ingredient);
+            }
+
+            let instructionsUl = document.createElement('ol');
+            instructionsUl.innerText = 'Instructions:'
+            for (let instr of instructions){
+                let instruction = document.createElement('li');
+                instruction.innerText = instr;
+                instructionsUl.appendChild(instruction);
+            }
+
+            let p = document.createElement('p');
+            p.innerText = `prepTimeMinutes: ${prepTimeMinutes} | cookTimeMinutes: ${cookTimeMinutes} | servings: ${servings} `;
+            let difficultyP = document.createElement('p');
+            difficultyP.innerText = `difficulty: ${difficulty} | cuisine: ${cuisine} | caloriesPerServing: ${caloriesPerServing}`;
+            let addInfo = document.createElement('p');
+            addInfo.innerText = `rating: ${rating} | reviewCount: ${reviewCount} | mealType: ${mealType} `
+
+            let img = document.createElement('img');
+            img.src = image;
+
+            recipeBlock.append(title, ingredientsUl, instructionsUl, p, difficultyP, addInfo, img);
+            container.appendChild(recipeBlock);
+        }
+        document.body.appendChild(container)
+
+
+    });
